da6caf7091b90568eb7e1119fb10e36bab43f9e9
Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
2019-11-14 02:36:07 +0000

    Prubas de instanciacion de metodo creadas y superadas.

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -1,15 +1,24 @@
 require 'alimento'
 
-RSpec.describe Alimento, "#atributos" do
+RSpec.describe Alimento do
 	before(:all) do
 		@instancia = Alimento.new()
 	end
 
 	context "Pruebas a los atributos" do
-		it "Pruebas de existencia de instancia en clase" do 
+		it "Pruebas de existencia de instancia en la clase" do 
 			expect(@instancia.instance_variables.include? :@nombre).to eq(true)
                         expect(@instancia.instance_variables.include? :@emision_gases).to eq(true)
                         expect(@instancia.instance_variables.include? :@terreno_utilizado).to eq(true)
 		end
 	end
+
+        context "Pruebas a los metodos" do
+                it "Pruebas de existencia de metodos en la clase" do
+                        expect(@instancia.methods.include? :nombre).to eq(true)
+                        expect(@instancia.methods.include? :emision_gases).to eq(true)
+                        expect(@instancia.methods.include? :terreno_utilizado).to eq(true)
+                end
+        end
+
 end
