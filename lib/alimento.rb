7a7f8b3da20dfb2285b17385fbec48f167bfe42c
Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
2019-11-14 02:21:17 +0000

    Superadas pruebas de instanciacion de atributos.

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -1,5 +1,8 @@
 class Alimento
+	attr_accessor :nombre, :emision_gases, :terreno_utilizado
 	def initialize()
-
+		@nombre = "a"
+		@emision_gases = 3
+		@terreno_utilizado = 4
 	end
 end
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -2,14 +2,14 @@ require 'alimento'
 
 RSpec.describe Alimento, "#atributos" do
 	before(:all) do
-		@intancia = Alimento.new()
+		@instancia = Alimento.new()
 	end
 
 	context "Pruebas a los atributos" do
 		it "Pruebas de existencia de instancia en clase" do 
-			expect(@instancia.instance_variables.include? :nombre).to eq(true)
-                        expect(@instancia.instance_variables.include? :emision_gases).to eq(true)
-                        expect(@instancia.instance_variables.include? :terreno_utilizado).to eq(true)
+			expect(@instancia.instance_variables.include? :@nombre).to eq(true)
+                        expect(@instancia.instance_variables.include? :@emision_gases).to eq(true)
+                        expect(@instancia.instance_variables.include? :@terreno_utilizado).to eq(true)
 		end
 	end
 end
