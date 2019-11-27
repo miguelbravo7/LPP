569368ed24e13cdb0e1781c6e4e2b7b6537ffc73
Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
2019-11-27 18:35:54 +0000

    Codigo comentado siguiendo la sintaxis de yard.

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -1,7 +1,26 @@
+# @title Modulo de la clase alimento.
+# Modulo en el que se ha implementado la clase Alimento
+# @author Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
 module Alimento
+
+  # Clase alimento que representa un alimento con sus propiedades nutricionales,
+  # su emision de gases y el terreno que se requiere para producir el alimento 
   class Alimento
+
+  	# Atributos de instancia que representan las propiedades del alimento
   	attr_accessor :nombre, :proteinas, :carbohidratos, :lipidos, :emision_gases, :terreno_utilizado
-  	attr_reader :to_kcal, :ing_rec
+
+	# Atributos de instancia que guardan los valores necesarios para hacer conversiones
+	attr_reader :to_kcal, :ing_rec
+
+	# Constructor de clase
+	# @param nombre [String] nombre del alimento
+	# @param proteinas [Numeric] numero de proteinas en gramos
+	# @param carbohidratos [Numeric] numero de carbohidratos en gramos
+	# @param lipidos [Numeric] numero de lipidos en gramos
+        # @param emision_gases [Numeric] numero de gases de efecto invernadero en kgCO2
+        # @param terreno_utilizado [Numeric] numero de metros cuadrados de uso al año en m2/año 
+	# @raise ArgumentError si los valores introducidos no son del tipo correcto
   	def initialize(nombre, proteinas, carbohidratos, lipidos, emision_gases, terreno_utilizado)
   		@nombre = nombre
   		@proteinas = proteinas
@@ -16,10 +35,15 @@ module Alimento
 		@ing_rec = {"hombre" => 3000.0, "mujer" => 2300.0}
 	end
 
+	# Metodo para representar la clase como cadena
+	# @return [String] cadena con el alimento y sus propiedades
 	def to_s()
 		"#{@nombre}: Proteinas:#{@proteinas}g Carbohidratos:#{@carbohidratos}g Lipidos:#{@lipidos}g GEI:#{@emision_gases}kgCO2eq Terreno:#{@terreno_utilizado}m^2anio"
 	end
 
+	# Metodo que calcula el porcentaje que aporta el alimento sobre la ingesta recomendada
+	# @params poblacion [String] poblacion en la que se calcula el porcentaje de ingesta recomendada
+	# @raise ArgumentError si no se tiene definida la poblacion definida
 	def por_ing_recomendada(poblacion)
 		raise StandardError, ("Parametro  tipo #{poblacion.class} no valido, usar String") unless poblacion.is_a? String
 		
@@ -32,6 +56,9 @@ module Alimento
 		end
 	end
 
+	# Metodo para el calculo del impacto ambiental
+	# @param (see #por_ing_recomendada)
+	# @raise (see #por_ing_recomendada)
 	def impacto_ambiental(poblacion)
 		(1 / por_ing_recomendada(poblacion) ) * @emision_gases
 	end
--- lib/alimento/lista.rb
+++ lib/alimento/lista.rb
@@ -1,17 +1,24 @@
+# Modulo en el que se ha implementado la clase Lista
+# @author Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
 module Alimento
 
+	 # Clase lista que representa una lista doblemente enlazada
 	class Lista
 		# Atributos de instancia
 		attr_reader :head, :tail, :size
-		# Atributos de clase
+
+		# Atributo de clase utilizado para instanciar los nodos que tiene la lista
 		@@Nodo = Struct.new(:data, :prev, :next)
 		
+		# Constructor de clase
 		def initialize()
 			@head = nil
 			@tail = nil
 			@size = 0
 		end
 
+		# Metodo para representar la clase como cadena
+		# @return [String] devuelve los valores como cadena que alberga la lista	
 		def to_s
        			current_node = @head
         		while current_node != nil
@@ -23,10 +30,13 @@ module Alimento
     		end
 
 		# Getter del atributo de clase Nodo
+		# @return [Struct] atributo nodo de la clase
 		def self.Nodo
 			@@Nodo
 		end
 		
+		# Iterador de clase para cada valor desde la cabeza
+		# @yield [Object] valor almacenado en el nodo actual
 		def each
 			cur_node = @head
 			while cur_node
@@ -36,6 +46,8 @@ module Alimento
 
     		end
 
+		# Inserta un valor al principio de la lista
+		# @param [Array<Object>, Object] valor o lista de valores a introducir
 		def insertar(value)
 			if value.respond_to? :each
 				value.each { |x| 
@@ -49,6 +61,8 @@ module Alimento
 		end
 
 		# Metodo del que depende el metodo insertar
+		# @see #insertar
+		# @return [true, false] en funcion de que se halla hecho la operacion o no
 		def insertarValor(value)
 			if !@tail.nil?
                                 @nodo = @@Nodo.new(value, @tail, nil)
@@ -63,6 +77,8 @@ module Alimento
 			true
 		end
 
+		# Extare el primer nodo de la lista
+		# @return [true, false] en funcion de que se halla hecho la operacion o no
 		def extraerPrimero
 			if @size < 2
 				if @head.nil?
@@ -82,6 +98,8 @@ module Alimento
 
 		end
 
+		# Extrae el ultimo nodo de la lista
+		# @return (see #extraerPrimero)
 		def extraerUltimo
                         if @size < 2
                                 if @tail.nil?
