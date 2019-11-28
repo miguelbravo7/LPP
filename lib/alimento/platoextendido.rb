bec44116217ea0cbd39fb15de8f0b44d4f3aee3a
Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
2019-11-28 03:18:14 +0000

    Superadas expectativas de la clase PlatoExtendido.

--- lib/alimento/platoextendido.rb
+++ lib/alimento/platoextendido.rb
@@ -1,6 +1,26 @@
 module Alimento
 	# Clase creada con el objetivo de cumplir el principio de sustitucion de Liskov
 	class PlatoExtendido < Plato
+		attr_reader :gases_invernadero
+		
+		def initialize(nombre, alimentos)
+			super(nombre, alimentos)
+			@gases_invernadero = 0
+			alimentos.each do |data|
+				 @gases_invernadero += data.emision_gases
+			end
+		end
 
+		def eficiencia_energetica
+
+		end
+
+		def uso_terreno
+			sum = 0
+			@alimentos.each do |data|
+				sum += data.terreno_utilizado
+			end
+			sum
+		end
 	end
 end
