9796bedce9277143a65d3b79d1e2b03aae7b8aea
Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
2019-12-19 01:43:00 +0000

    Expectativas superadas.

--- lib/alimento/menu.rb
+++ lib/alimento/menu.rb
@@ -1,11 +1,21 @@
-include 'alimento.rb'
+# Implementacion de la clase PlatoExtendido
+# @author Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
 
 module Menu
+	class Menu
+                # Atributos de instancia que representan las propiedades del menu
+		attr_accessor :precios, :platos
 
-	class Menu < Array
-
+		# Constructor de clase
 		def initialize()
-			super()
+			@precios = Array.new()
+			@platos = Array.new()
 		end
+
+		def anadir_plato(plato, precio)
+			@platos << plato
+			@precios << precio
+		end
+
 	end
 end
--- lib/alimento/plato.rb
+++ lib/alimento/plato.rb
@@ -1,6 +1,16 @@
 # Implementacion de la clase Plato 
 # @author Miguel Bravo Arvelo <alu0101031538@ull.edu.es>
 module Plato
+	INDICE_BAJO = 1
+        INDICE_MEDIO = 2
+        INDICE_ALTO = 3
+
+        ENERGIA_BAJO = 670
+        ENERGIA_MEDIO = 830
+
+        GASES_BAJO = 800
+        GASES_MEDIO = 1200
+
 	# Clase Plato que representa un un grupo de alimentos con sus propiedades nutricionales
 	class Plato
 		include Comparable
@@ -33,6 +43,59 @@ module Plato
 		def <=>(obj)
 			[@cjto_gramos, @alimentos] <=> [obj.cjto_gramos, obj.alimentos]
 		end
+
+                # Metodo que calcula el sumatorio de energia del plato
+                # @return [Numeric] energia obtenida del total de alimentos
+                def energia()
+			sum = 0
+                        @alimentos.each do |data|
+                        	sum += data.kcal_sum()
+                        end
+			sum
+                end
+
+		# Metodo que calcula el sumatorio de emision de gases del plato
+                # @return [Numeric] emision de gases obtenida del total de alimentos
+                def emision_gases()
+                        sum = 0
+                        @alimentos.each do |data|
+                                sum += data.emision_gases
+                        end
+                        sum
+                end
+
+                # Metodo que calcula la huella nutricional del plato
+                # @return [Numeric] huella nutricional obtenida del total de alimentos
+                def huella_nutricional()
+                        impacto_energia() + impacto_emision_gases()
+                end
+
+		# Metodo que calcula impacto de la energia del plato
+                # @return [Numeric] impacto obtenido del total de alimentos
+                def impacto_energia()
+			sum = energia()	
+                        if sum < ENERGIA_BAJO
+                                INDICE_BAJO
+                        elsif sum < ENERGIA_MEDIO
+                                INDICE_MEDIO
+                        else
+                                INDICE_ALTO
+                        end
+                end
+
+		# Metodo que calcula impacto de la emision de gases del plato
+                # @return [Numeric] impacto obtenido del total de alimentos
+                def impacto_emision_gases()
+                        sum = emision_gases()                        
+                        if sum < GASES_BAJO
+                                INDICE_BAJO
+                        elsif sum < GASES_MEDIO
+                                INDICE_MEDIO
+                        else
+                                INDICE_ALTO
+                        end
+
+                end
 
 		# Metodo que calcula el porcentaje de proteinas del plato
 		# @return [Numeric] porcentaje obtenido del total de alimentos
